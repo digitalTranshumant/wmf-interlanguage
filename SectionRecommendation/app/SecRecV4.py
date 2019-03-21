72177cbe71de760480f73f7d77e9a9271126a53f
Diego <dsaez@diegotest.wmf-research-tools.eqiad.wmflabs>
2019-03-21 16:17:54 +0000

    Fixing bugs

---
+++ SectionRecommendation/app/SecRecV4.py
@@ -0,0 +1,374 @@
+
+# coding: utf-8
+
+# # Section Recommendation / Inter-lingual approach
+# 
+# This notebook implements section recommendation based on the Section Aligments. 
+# Given an article in language a target language T, though Wikidata retrieves the section of the same in article in other languages, and provides a recommendations.
+
+# In[1]:
+
+
+import pandas as pd
+import requests
+import re
+
+#Config
+suportedLangs = ['fr','en','es','ja','ar','ru']
+
+
+# In[2]:
+
+
+#Loads alignments 
+
+import os 
+rec = {}
+for f in os.listdir('recSheetsTSV/'):
+    lang1 = f[0:2]
+    lang2 = f[6:8]
+    rec[lang1] = rec.get(lang1,{})
+    rec[lang1][lang2] = pd.read_csv('recSheetsTSV/'+f,sep='\t')
+    rec[lang1][lang2].set_index('secFrom',inplace=True)
+    
+
+
+# In[43]:
+
+
+#This function is for the aligment API, I don't use in the recommendations
+def getAlignment(lang1,lang2,sec):  
+    df = rec[lang1][lang2]
+    tmp = df[df.index ==sec][['langTo','prob']]
+    return list(zip(tmp['langTo'],tmp['prob']))
+
+
+# In[81]:
+
+
+## Section parser
+#sections_RE = re.compile(r'(^|[^=])==([^=\n\r]+)==([^=]|$)') #I've used this expression in the paper
+#but it doesn't get subsections, now, changing for a more general one
+sections_RE = re.compile(r'==([^=\n\r]+)==')
+def extract_sections(text):
+    for m in sections_RE.finditer(text):
+        #yield m.group(2).strip() #this is for the old regular expression
+        yield m.group(1).strip()
+        
+#Get articles
+def getContent(title,lang):
+    url = "https://%s.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&formatversion=2&titles=%s" % (lang,title)
+    response = requests.get(url)
+    content = response.json()['query']['pages'][0]['revisions'][0]['content']
+    return content
+
+def getPages(title,lang,target=suportedLangs):
+    """
+    title: page title in target language
+    lang: target language
+    target: List of Pages 
+    returns a dictionary 'x_wiki':x_title
+    """
+    response= requests.get("https://www.wikidata.org/w/api.php?action=wbgetentities&sites=%swiki&titles=%s&props=sitelinks&format=json" % (lang,title))
+    output ={}
+    assert list(response.json()['entities'].values())[0]['sitelinks'], "Oh no! This assertion failed!"
+    links = list(response.json()['entities'].values())[0]['sitelinks']
+    for t in target:
+        if t+'wiki' in links:
+            output[t] = links[t+'wiki' ]['title']
+        
+    return output
+
+def getSectionsOnline(title,lang):
+    #This function gets the sections directly from the 
+    #mediawikiparser
+    #For offline use list(extract_sections(getContent(page,l)))
+    response = requests.get("https://%s.wikipedia.org/w/api.php?action=parse&page=%s&format=json" % (lang,title))
+    r = response.json()
+    return [s['line'] for s in r['parse']['sections']]
+
+def getAllLangs(title,lang,online=False,giveTitles=False): #offline, meaning not using the mediawiki parser, is 3 times faster
+    """
+    title: page title in target language
+    lang: target language
+    returns a dictionary 'x_wiki':list_of_sections_in_x 
+    """
+    titles ={}
+    secs = {}
+    for l,page in getPages(title,lang).items():
+        titles[l] = page
+        #print(l,page)
+        if online:
+            secs[l] = getSectionsOnline(page,l)
+        else:
+            secs[l]  = list(extract_sections(getContent(page,l)))
+    if giveTitles:
+        return secs,titles
+    else:
+        return secs
+
+# In[82]:
+
+
+import networkx as nx
+import json
+
+#For each section in language X, we have N possible mappings with certain probability. 
+#Ex: 'Vida temprana' in Spanish maps to 'Early Life,p=0.9','Early Years,p=.8','References,p=0.3' in English
+#Given two languages X and Y, we want to find the most similar clusters considering the mapped sections
+def getMostSimilarClusters(c1,c2): 
+    """
+    c1: dictionary of sections in language 1 (allready mapped to target language) with a giving probability
+    c2: dictionary of sections in language 2 (allready mapped to target languages) with a giving probability
+    """
+    G = nx.Graph()
+    mostSimilar = []
+    for pos1,dict_1 in enumerate(c1):
+        for pos2,dict_2 in enumerate(c2):
+            dot_product = sum(dict_1[key]*dict_2.get(key, 0) for key in dict_1)
+            G.add_edge((1,pos1),(2,pos2))
+            G[(1,pos1)][(2,pos2)]['w'] = dot_product
+    for a, b, data in sorted(G.edges(data=True), key=lambda x: x[2]['w'],reverse=True):
+        try:
+            G.remove_node(a)
+            G.remove_node(b)   
+            sortedNodes  = sorted([a,b])
+            mostSimilar.append((sortedNodes[0][1],sortedNodes[1][1]))
+        except:
+            pass #one of the nodes was already paried
+    return mostSimilar
+
+#Given a template dictionary of sections c1
+#and another dictionary of sections c2
+#update  probabilities on c1 based on information c2
+def updateClusterWeights(c1,c2): #c1 is the template to be updated
+    """
+    c1: dictionary of sections in language 1 (allready mapped to target language) with a giving probability
+    c2: dictionary of sections in language 2 (allready mapped to target languages) with a giving probability
+    """
+    for s1,s2 in getMostSimilarClusters(c1,c2):
+        for s in set(c1[s1].keys()) & set(c2[s2].keys()):
+            c1[s1][s] = c1[s1][s] + c2[s2][s]
+    return c1
+        
+
+
+# In[5]:
+
+
+def getRecs(title,TargetLang,blind=False,verbose=False,giveTitles=False):
+    '''
+    title: Article to get recommendations (ex:'Quilombo')
+    TargetLang: Language of the article (ex:'en')
+    verbose: return explanations
+    '''
+    #load translations
+    global rec
+    # get sections in all Languages
+    if giveTitles:
+        secs,titles = getAllLangs(title,TargetLang,giveTitles=giveTitles)
+    else:
+        secs = getAllLangs(title,TargetLang)
+    # get a list of the sources languages
+    sourceLangs = set(secs.keys()) - {TargetLang}
+    #count amount of sections in the target language
+    lenTarget = len(secs[TargetLang])
+    #count the number of sections in each source lang, produce a tuple (SecCount,Lang)
+    lenSources = [(len(s),l) for l,s in secs.items() if l != TargetLang]
+    #use the language with more sections as template
+    templateLang = max(lenSources)[1]
+    secsMapped = {}
+    #For all source languages S, take all sections in S and map to the target languge T, with it's probability
+    for lang in sourceLangs:
+        df = rec[lang][TargetLang]
+        secsMapped[lang] = []
+        for sec in secs[lang]:
+            tmp = df[df.index ==sec][['langTo','prob']]
+            secsMapped[lang].append(dict(zip(tmp.langTo,tmp.prob)))
+    #Use the language with more sections as template
+    templateRec = secsMapped[templateLang]
+    #Update the template using the remaining languages
+    for lang in sourceLangs:
+        if lang != templateLang:
+            templateRec = updateClusterWeights(templateRec,secsMapped[lang])
+    finalRecs = []
+    for cluster in templateRec:
+        if cluster: #check cluster is not empty
+            candidates = [recTuple[0] for recTuple in sorted(cluster.items(),  key=lambda x: x[1],reverse=True)][:3]
+            if not blind:
+                if not (set(candidates) & set(secs[TargetLang])):
+                    finalRecs.append(candidates[0])
+            else:
+                    finalRecs.append(candidates[0])
+
+    if verbose:
+        output = {}
+        output['context'] = {}
+        output['Recommendations'] = list(set(finalRecs))
+        output['context']['CurrentSections'] = secs[TargetLang]
+        #create a copy of sections
+        otherLangs = secs.copy()
+        del(otherLangs[TargetLang])
+        output['context']['SectionsInOtherLanguages'] = otherLangs
+        if giveTitles:
+            del(titles[TargetLang])
+            output['context']['titles'] = titles
+            
+    else:
+        output =list(set(finalRecs))
+    return output
+
+### From here to solve the Insect problem
+
+def getWikidataId(title,lang):
+    response= requests.get("https://www.wikidata.org/w/api.php?action=wbgetentities&sites=%swiki&titles=%s&props=sitelinks&format=json" % (lang,title))
+    return list(response.json()['entities'].keys())[0]
+
+def getWikidataSimilar(wikidataid,numOfItems=5):
+    url = "https://www.wikidata.org/w/api.php?action=query&list=search&srsearch=morelike:%s&srlimit=%s&srprop=size&formatversion=2&format=json" % (wikidataid,numOfItems)
+    response = requests.get(url)
+    similarItems = []
+    for element in response.json()['query']['search']:
+        similarItems.append(element['title'])
+    return similarItems
+
+def getMoreLikeSeeds(wikidataIds,target=suportedLangs):
+	url = "https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&props=sitelinks&ids=%s&sitefilter=%swiki" % ('|'.join(wikidataIds),'wiki|'.join(target))
+	response = requests.get(url).json()
+	seeds = []
+	for Q,data in response['entities'].items():
+		try:
+			numLangs = len(list(data['sitelinks'].values()))
+			seed = list(data['sitelinks'].values())[0]
+			seeds.append((numLangs,Q,seed['site'][0:2],seed['title'])) #return (numOfLangs, Q, lang, title), i just store one title-lang because it works as seed
+		except:pass
+	return seeds
+
+#This fuction is second version of getRecs
+#Designed to solve the insect problem (stubs with no sections)
+#This fuction is second version of getRecs
+#Designed to solve the insect problem (stubs with no sections)
+def getRecs2(title,TargetLang,auxLang=False,blind=False,verbose=False,giveTitles=False,useMoreLike=True):
+    '''
+    #This fuction is second version of getRecs
+    #Designed to solve the insect problem (stubs with no sections)
+    #When no sections are found, the function call the morelike api on Wikidata
+    #And uses a similar wikidaItem to provide the recs
+    title: Article to get recommendations (ex:'Quilombo')
+    TargetLang: Language of the article (ex:'en')
+    verbose: return explanations
+    blind: does not consider existing sections
+    auxLang: is used for useMoreLike, to provide the seed in other than the target language
+    useMoreLike: implement the search on morelike wikidaitems when no sections are found
+    
+    '''
+    #load translations
+    global rec
+    # get sections in all Languages
+    if giveTitles:
+        if auxLang:
+            secs,titles = getAllLangs(title,auxLang,giveTitles=giveTitles)
+        else:
+            secs,titles = getAllLangs(title,TargetLang,giveTitles=giveTitles)
+    else:
+        secs = getAllLangs(title,TargetLang)
+    # get a list of the sources languages
+    sourceLangs = set(secs.keys()) - {TargetLang}
+    #count amount of sections in the target language
+    lenTarget = len(secs[TargetLang])
+    #count the number of sections in each source lang, produce a tuple (SecCount,Lang)
+    lenSources = [(len(s),l) for l,s in secs.items() if l != TargetLang]
+    if useMoreLike: #Check if there no sections to recommend
+        if not secs[TargetLang]:
+            if not lenSources:
+                morelikeSeeds= getMoreLikeSeeds(getWikidataSimilar(getWikidataId(title,TargetLang)))
+                temporalRec = {'Recommendations':[]}
+                for seed in sorted(morelikeSeeds,reverse=True):
+                    try:
+                        l,p = seed[2],seed[3] #(numOfLangs, Q, lang, title)
+                        recs = getRecs2(p,TargetLang,auxLang=l,verbose=verbose,blind=True,giveTitles=True,useMoreLike=False)
+                        if recs['Recommendations']:
+                            recs['Recommendations'].extend(recs['context']['CurrentSections']) # add current sections 
+                            recs['Recommendations'] = list(set(recs['Recommendations'])) #remove duplicates
+                        if len(recs['Recommendations']) > len(temporalRec['Recommendations']):
+                            temporalRec = recs
+                    except:
+                        pass
+                if temporalRec['Recommendations']:
+                    if verbose:
+                        return temporalRec
+                    else:
+                        temporalRec['Recommendations']
+    #use the language with more sections as template   
+    if lenSources:
+        templateLang = max(lenSources)[1]
+        secsMapped = {}
+        #For all source languages S, take all sections in S and map to the target languge T, with it's probability
+        for lang in sourceLangs:
+            df = rec[lang][TargetLang]
+            secsMapped[lang] = []
+            for sec in secs[lang]:
+                tmp = df[df.index ==sec][['langTo','prob']]
+                secsMapped[lang].append(dict(zip(tmp.langTo,tmp.prob)))
+        #Use the language with more sections as template
+        templateRec = secsMapped[templateLang]
+        #Update the template using the remaining languages
+        for lang in sourceLangs:
+            if lang != templateLang:
+                templateRec = updateClusterWeights(templateRec,secsMapped[lang])
+        finalRecs = []
+        for cluster in templateRec:
+            if cluster: #check cluster is not empty
+                candidates = [recTuple[0] for recTuple in sorted(cluster.items(),  key=lambda x: x[1],reverse=True)][:3]
+                if not blind:
+                    if not (set(candidates) & set(secs[TargetLang])):
+                        finalRecs.append(candidates[0])
+                else:
+                        finalRecs.append(candidates[0])
+    else:
+        finalRecs = []
+    if verbose:           
+        output = {}
+        output['context'] = {}
+        if auxLang: #auxLang is just used in second iteration of useMoreLike
+            output['context']['usedMoreLike'] = {auxLang:title}
+        else:
+            output['context']['usedMoreLike'] = False
+        output['Recommendations'] = list(set(finalRecs))
+        output['context']['CurrentSections'] = secs[TargetLang]
+        #create a copy of sections
+        otherLangs = secs.copy()
+        del(otherLangs[TargetLang])
+        output['context']['SectionsInOtherLanguages'] = otherLangs
+        if giveTitles:
+            del(titles[TargetLang])
+            output['context']['titles'] = titles
+            
+    else:
+        output = list(set(finalRecs))
+    return output
+
+
+
+def getRandom(lang,num=500):
+    """
+    lang: str, language to get random article
+    num: number of articles to be retreive
+    """
+    url = "https://%s.wikipedia.org/w/api.php?format=json&action=query&list=random&rnnamespace=0&rnlimit=%s" % (lang,num)
+    response = requests.get(url)
+    content = response.json()['query']['random']
+    return content
+
+def getArticleWithRec(lang):
+	recs = False
+	articles = getRandom(lang)
+	while not recs:
+		try:
+			article = articles.pop()
+			results = getRecs2(article['title'],lang)
+			recs = results
+		except:
+			pass
+	return article['title']
+
--- SectionRecommendation/app/test.py
+++ SectionRecommendation/app/test.py
@@ -1,14 +1,16 @@
 from bottle import route, run, template,request,static_file
-import SecRecV3 as SecRec
+import SecRecV4 as SecRec
 import json
 import requests
 import mysql.connector as mariadb
 
 f = open('server.log','a',buffering=1)
-mariadb_connection = mariadb.connect(user='app', password='pool', database='app')
-cursor = mariadb_connection.cursor() 
+
+
 
 # create TABLE evaluation (    ip VARCHAR(100),    article VARCHAR(200) ,    lang VARCHAR(40),    section VARCHAR(40),    evaluation VARCHAR(40), ts TIMESTAMP );
+
+# create TABLE evaluationGeneral (    ip VARCHAR(100),    article VARCHAR(200) ,    lang VARCHAR(40),    Q1 VARCHAR(40),    Q2 VARCHAR(40), Q3 VARCHAR(40),Q4 VARCHAR(40),Q5 VARCHAR(40),ts TIMESTAMP );
 
 
 #Sec Rec API
@@ -23,7 +25,7 @@ def APIRecs(lang,title):
 		error = {'error':'%s is not supported; supported languages are: %s' % (lang,','.join(SecRec.suportedLangs))}
 		return error
 	try:
-		recs = SecRec.getRecs(title,lang,blind=blind,verbose=verbose)
+		recs = SecRec.getRecs2(title,lang,blind=blind,verbose=verbose)
 		return template('{{name}}', name=recs)
 	except:
 		error = {'error':'%s not found, please try with another article' % title}
@@ -48,8 +50,8 @@ def APIAlignment(lang1,lang2,section):
 def server_static(filename):
 	return static_file(filename, root='.')
 
-@route('/')
-def index():
+@route('/v1')
+def indexv1():
 	title = request.query.title or 'Quilombo'
 	lang = request.query.lang or 'en'
 	userIp = request.environ.get('HTTP_X_FORWARDED_FOR')
@@ -62,9 +64,9 @@ def index():
 			response = requests.get("https://%s.wikipedia.org/api/rest_v1/page/summary/%s" % (lang,title))
 			r = response.json()
 			text = r['extract_html'] 
-			image =  r['thumbnail']['source']
-			recs = SecRec.getRecs(title,lang,blind=False,verbose=True,giveTitles=True)
-			secRec = eval(recs['Recommendations'])
+			image =  r.get('thumbnail', {}).get('source','')
+			recs = SecRec.getRecs2(title,lang,blind=False,verbose=True,giveTitles=True)
+			secRec = recs['Recommendations']
 			currentSec = recs['context']['CurrentSections']
 			numPerLang = {}
 			langtitles = recs['context']['titles']
@@ -75,12 +77,12 @@ def index():
 			try:    #this try is in case that SecRec.getRecs fails, but there is still a pagepreview
 				response = requests.get("https://%s.wikipedia.org/api/rest_v1/page/summary/%s" % (lang,title))
 				r = response.json()
-				text = r['extract_html'] 
-				image =  r['thumbnail']['source']
+				text = r['extract_html']
+				image =  r.get('thumbnail', {}).get('source','')
 			except:
 				text = 'Article not found, please try another article'
 				image = ''
-
+			recs = {'context':{'usedMoreLike':''}}
 			secRec = []
 			currentSec = ''
 			langtitles = {}
@@ -184,6 +186,9 @@ def index():
 
 					</ul>
 					<b>Number of sections in other languages:</b>
+					%if useMoreLike:
+					<br>We couldn't find sections to recommend in any of the supported languages. This page might be an <a href="https://en.wikipedia.org/wiki/Wikipedia:Stub"> Stub</a>. To provide recommendations we have used similar pages:	<br> 
+					%end
 					<ul>
 					  % if not numPerLang:
 						<li> These article does not exists in any of the other supported languages </li>
@@ -194,7 +199,7 @@ def index():
 					</ul>
 					<b>Current sections and subsections in this article are:</b>
 					<ul>
-					  %if currentSec:
+					  %if currentSec and not useMoreLike:
 					 <li>{{currentSec}} </li>
 					 %end	
 					  %if not currentSec:
@@ -210,7 +215,7 @@ def index():
 
 			</body>
 			</html>
-			''', text=text,image=image,title=title,lang=lang,secRec=secRec,currentSec=currentSec,numPerLang=numPerLang,langtitles=langtitles)
+			''', text=text,image=image,title=title,lang=lang,secRec=secRec,currentSec=currentSec,numPerLang=numPerLang,langtitles=langtitles,useMoreLike=recs['context']['usedMoreLike'])
 
 
 ### Evaluated
@@ -297,5 +302,229 @@ def evaluated(lang,title):
 			</html> ''')
 
 
+## sandbox
 
-run(host='0.0.0.0',port=80)
+@route('/')
+def index():
+	try:
+		evaluations = dict(request.query.decode())
+		if evaluations.get('GenerateRandom',False):
+			title = SecRec.getArticleWithRec('en')
+			lang =  'en'
+		else:
+			title = request.query.title or SecRec.getArticleWithRec('en')
+			lang = request.query.lang or 'en'
+		print(title,lang)
+		userIp = request.environ.get('HTTP_X_FORWARDED_FOR')
+		f.write('Demopage %s %s %s \n' % (userIp,lang,title))
+		if evaluations:
+			mariadb_connection = mariadb.connect(user='app', password='pool', database='app')
+			titleEval = evaluations.get('title')
+			langEval = evaluations.get('lang')
+			Q1 = evaluations.get('Q1','No Reponse')
+			Q2 = evaluations.get('Q2','No Reponse')
+			Q3 = evaluations.get('Q3','No Reponse')
+			Q4 = evaluations.get('Q4','No Reponse')
+			Q5 = evaluations.get('Q5','No Reponse')
+			print('values:',userIp, titleEval, langEval,Q1,Q2,Q3,Q4,Q5)
+			cmd = "INSERT INTO evaluationGeneral (ip,article,lang,Q1,Q2,Q3,Q4,Q5) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s')" % (userIp, titleEval, langEval,Q1,Q2,Q3,Q4,Q5)
+			print(cmd)
+			cursor = mariadb_connection.cursor() 
+			cursor.execute(cmd)
+			mariadb_connection.commit() 
+			cursor.close()
+			mariadb_connection.close()
+
+		if lang not in SecRec.suportedLangs:
+			text = '%s is not supported; supported languages are: %s' 
+		else:
+
+			try:
+				response = requests.get("https://%s.wikipedia.org/api/rest_v1/page/summary/%s" % (lang,title))
+				r = response.json()
+				text = r['extract_html'] 
+				image =  r.get('thumbnail', {}).get('source','')
+				recs = SecRec.getRecs2(title,lang,blind=False,verbose=True,giveTitles=True)
+				secRec = recs['Recommendations']
+				currentSec = recs['context']['CurrentSections']
+				numPerLang = {}
+				langtitles = recs['context']['titles']
+				for l, s in  recs['context']['SectionsInOtherLanguages'].items():
+					numPerLang[l] = len(s)
+
+			except:
+				try:    #this try is in case that SecRec.getRecs fails, but there is still a pagepreview
+					response = requests.get("https://%s.wikipedia.org/api/rest_v1/page/summary/%s" % (lang,title))
+					r = response.json()
+					text = r['extract_html'] 
+					image =  r.get('thumbnail', {}).get('source','')
+				except:
+					text = 'Article not found, please try another article'
+					image = ''
+				recs = {'context':{'usedMoreLike':''}}
+				secRec = []
+				currentSec = ''
+				langtitles = {}
+				numPerLang = {}
+		return template('''  
+
+				<!DOCTYPE html>
+				<html>
+				<head>
+
+				  <title>Section Recommendation Demo</title>
+				  <link href="/static/bootstrap.min.css" rel="stylesheet">
+				<style>
+					body {
+					    background-color: #EAECF0 !important;
+					}
+
+					header, main, footer {
+					    margin: 10px 0 30px;
+					}
+
+					header .project-name {
+					    font-size: 1.5em;
+					}
+
+					footer {
+					    margin-top: 100px;
+					}
+
+								
+					.main-content {
+					    background-color: white;
+					    padding: 15px;
+					}
+
+				p {
+				  text-align: justify;
+				  text-justify: inter-word;
+				}
+				</style>
+
+
+				</head>
+
+				<body>
+				  <div class="container">
+
+				   <div class="row" style="border-style:solid">  
+					<form action="/">
+				<table >
+					 <tr> <td >Wiki:  </td><td style="margin:5px">
+					  <select name="lang" value="{{lang}}">
+					    <option value="en" selected ></option>
+					    <option value="ar">ar.wikipedia</option>
+					    <option value="en" selected="selected">en.wikipedia</option>
+					    <option value="es">es.wikipedia</option>
+					    <option value="fr">fr.wikipedia</option>
+					    <option value="ja">ja.wikipedia</option>
+					    <option value="ru">ru.wikipedia</option>
+					  </select> <br></td> 
+			
+					 <td >Article:</td><td> <input type="text" name="title" value="{{title}}"> <br> </td> 
+					 <td><input type="submit" value="Get recommendations"> <br></td> </tr>
+								</form>  
+				</table>
+
+				   </div>
+			
+				   <div class="row justify-content-md-center">
+	   				
+					<div class="col-lg">
+	  			  	         <h3> {{title}} - {{lang}}.wikipedia.org </h3>  
+						 <b> Article Summary: </b> <br>
+
+
+						<img style="margin: 0px 15px 5px 0px" src="{{image}}" ALIGN="left"> {{!text[:-4]}} 
+	<a href='https://{{lang}}.wikipedia.org/w/index.php?title={{title}}' target='_blank'> View on Wikipedia</a> </p>
+
+
+					<b>Current sections and subsections in this article are:</b>
+					<ul>
+					%if currentSec and not useMoreLike:
+						%for current in currentSec:
+						<li>{{current}} </li>
+						%end
+					%end	
+					%if not currentSec:
+					<li>The current article has no sections yet. </li>
+					%end
+					</ul>
+
+
+					</div>
+
+					<div class="col-sm">
+
+						<h3>Recommended sections:</h3>
+						<form action="/">
+						<ul>
+						% for item in secRec:
+							<li><b>{{item}}</b> </li>				
+						%end
+						</ul>
+
+
+
+						 %if secRec:
+
+						<b> Most or all of the recommended sections.. </b>
+						<ol>
+						<li>  Are relevant to the topic of this article </li>
+						<input type="radio" name="Q1" value="agree" > Agree <input type="radio" name="Q1" value="disagree" > Disagree
+						<li> Are important for this article to have</li>
+						<input type="radio" name="Q2" value="agree" > Agree <input type="radio" name="Q2" value="disagree" > Disagree
+						<li> Are redundant or conflict with the existing sections in the article</li>
+						<input type="radio" name="Q3" value="agree" > Agree <input type="radio" name="Q3" value="disagree" > Disagree
+						<li> Are redundant or conflict with other recommended sections</li>
+						<input type="radio" name="Q4" value="agree" > Agree <input type="radio" name="Q4" value="disagree" > Disagree
+						</ol>
+						<ul>
+						<li> <b> If I were expanding this article, I would find these recommendations useful </b></li>
+						<input type="radio" name="Q5" value="agree" > Agree <input type="radio" name="Q5" value="disagree" > Disagree <br>
+						 <input type="submit" value="Submit Ratings"> <br>
+						 %end
+						 %if not secRec:
+						 We can't provide section recommendations for this article.  This might be because the current article is the one with more complete set of sections among all the supported languages, please select another article.
+						 %end
+	   					 <input type="hidden" name="title" value="{{title}}" />
+	   					 <input type="hidden" name="lang" value="{{lang}}" />
+	   					 <input type="hidden" name="GenerateRandom" value="yes" />
+						  </form> 
+						<form action="/"> <input type="submit" value="Skip Article"> <br>
+						</ul>
+	<b> Provide additional feedback </b> <br>
+	<p>If you would like to provide additional feedback on
+	these recommendations, please do so on the <a href='https://meta.wikimedia.org/wiki/Research_talk:Expanding_Wikipedia_articles_across_languages/Inter_language_approach/Feedback' target="_blank">talk page</a>. </p>
+	<p>If you paste the template below with your feedback, it
+	will help us understand your comments better.</p>
+
+	{{'{{'}}Section_recommendation_feedback <br>
+	|lang={{lang}}<br>
+	|article={{title}}<br>
+	|link=https://secrec.wmflabs.org/?lang={{lang}}&title={{title}}<br>
+	% for n,item in enumerate(secRec):
+	|section{{n+1}}={{item}}<br>				
+	%end
+	{{'}}'}}				</div>
+				    </div>
+	
+
+				   </div>
+
+				</body>
+				</html>
+				''', text=text,image=image,title=title,lang=lang,secRec=secRec,currentSec=currentSec,numPerLang=numPerLang,langtitles=langtitles,useMoreLike=recs['context']['usedMoreLike'])
+
+	except Exception as e: 
+		print('error - reloading',e)
+		index()
+		
+
+
+
+
+
+run(host='0.0.0.0',port=80,server='paste',reloader=True,interval=20,quiet=False)
